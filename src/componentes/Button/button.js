10424ca85c908927450b7e1a62c4994ec2242c08
Alejo_sanchez <alejoh908@gmail.com>
2023-09-29 21:23:30 -0500

    Add File dolowand

--- src/componentes/Button/button.js
+++ src/componentes/Button/button.js
@@ -1,11 +1,32 @@
+'use client'
 import "./button.css"
+
+
 function Button() {
+  const downloadFile = () => {
+    fetch('abp.apk').then(response => {
+      response.blob().then(blob => {
+          // Creating new object of PDF file
+          const fileURL = window.URL.createObjectURL(blob);
+          // Setting various property values
+          let alink = document.createElement('a');
+          alink.href = fileURL;
+          alink.download = 'abp.apk';
+          alink.click();
+      })
+  })
+   
+  }
   return (
-    <button
-    className="buttonDolloawn"
-    >
-    Descargar
-    </button>
+   
+      <button
+        className="buttonDolloawn"
+        onClick={() => downloadFile()}
+      >
+        Descargar
+      </button>
+   
+
   )
 }
 
